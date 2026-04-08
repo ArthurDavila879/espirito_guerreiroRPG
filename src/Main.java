d96373d66142c51e9f5fa066450b372f5da3292e
Cauapassos <cauapassos470@gmail.com>
2026-04-07 21:48:55 -0300

    teste01

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import domain.Oraculo;
+
 public class Main {
     public static void main(String[] args) {
-
+        Oraculo Oraculo_master = new Oraculo();
+        Oraculo_master.definirNome("arthurdavila");
     }
 }
